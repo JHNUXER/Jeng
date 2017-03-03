43714656068d04f8f179dbbde2b4abb3d38c53c2
John Kayne <johnhoganuke2@gmail.com>
2017-03-03 12:30:31 -0500

    Update Tile.java

--- rg0/src/main/java/jeng/world/tile/Tile.java
+++ rg0/src/main/java/jeng/world/tile/Tile.java
@@ -8,6 +8,8 @@ public abstract class Tile {
   protected TileSlot slot;
   protected float[] health;
   
+  public abstract boolean destroy();
+  
   public void tick() {
     if (this.health[0] == 0) {
       slot.destroyTile();
