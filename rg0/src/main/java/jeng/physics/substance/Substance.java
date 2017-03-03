49506bfc0c0dee0328886b63d09a7871e5b28d68
John Kayne <johnhoganuke2@gmail.com>
2017-03-03 12:32:59 -0500

    Update Substance.java

--- rg0/src/main/java/jeng/physics/substance/Substance.java
+++ rg0/src/main/java/jeng/physics/substance/Substance.java
@@ -1,5 +1,11 @@
 package jeng.physics.substance;
 
 public class Substance {
-  pu
+  public String name;
+  public String id;
+  public float density;
+  
+  public float getMass(float volume) {
+    return volume * density;
+  }
 }
